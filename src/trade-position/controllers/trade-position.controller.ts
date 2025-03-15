912bbce840e029392ab627bc589622864a1ee1a4
MUGISHA-Pascal <mugishapascal2008@gmail.com>
2025-03-16 00:30:28 +0200

    adding the trade/position controllers

--- src/trade-position/controllers/trade-position.controller.ts
+++ src/trade-position/controllers/trade-position.controller.ts
@@ -6,6 +6,7 @@ import {
   Patch,
   Param,
   Delete,
+  Put,
 } from '@nestjs/common';
 import { CreateTradePositionDto } from '../dto/create-trade-position.dto';
 import { UpdateTradePositionDto } from '../dto/update-trade-position.dto';
@@ -14,8 +15,28 @@ import { TradePositionService } from '../services/trade-position.service';
 @Controller('trade-position')
 export class TradePositionController {
   constructor(private readonly tradePositionService: TradePositionService) {}
-  @Post()
+  @Post('add')
   async addTrade(@Body() createTrade: CreateTradePositionDto) {
     return await this.tradePositionService.addTrade(createTrade);
+  }
+  @Get('get/:id')
+  async getTrade(@Param('id') id: string) {
+    return await this.tradePositionService.getTrade(id);
+  }
+  @Get('get/trades')
+  async getTrades() {
+    return await this.tradePositionService.getTrades();
+  }
+  @Delete('delete/:id')
+  async deleteTrade(@Param('id') id: string) {
+    return await this.tradePositionService.deleteTrade(id);
+  }
+  @Put('edit')
+  async editTrade(@Body() updateTrade: UpdateTradePositionDto) {
+    return await this.tradePositionService.editTrade(updateTrade);
+  }
+  @Get('number')
+  async getNumber() {
+    return await this.tradePositionService.getTradesNumber();
   }
 }
