3b90ebe731b0efd996ba6eb7a49e91689fd08543
MUGISHA-Pascal <mugishapascal2008@gmail.com>
2025-03-17 14:53:16 +0200

    adding the location controllers

--- src/location/controllers/location.controller.ts
+++ src/location/controllers/location.controller.ts
@@ -6,6 +6,7 @@ import {
   Patch,
   Param,
   Delete,
+  Put,
 } from '@nestjs/common';
 import { CreateLocationDto } from '../dto/create-location.dto';
 import { UpdateLocationDto } from '../dto/update-location.dto';
@@ -14,4 +15,24 @@ import { LocationService } from '../services/location.service';
 @Controller('location')
 export class LocationController {
   constructor(private readonly locationService: LocationService) {}
+  @Get('locations')
+  async getLocations() {
+    return await this.locationService.getLocations();
+  }
+  @Get(':id')
+  async getLocationById(@Param('id') id: string) {
+    return await this.locationService.getLocationById(id);
+  }
+  @Post('add')
+  async addLocation(@Body() createLocation: CreateLocationDto) {
+    return await this.locationService.addLocation(createLocation);
+  }
+  @Put('edit')
+  async editLocation(@Body() updateLocation: UpdateLocationDto) {
+    return await this.locationService.editLocation(updateLocation);
+  }
+  @Delete(':id')
+  async deleteLocation(@Param('id') id: string) {
+    return await this.locationService.deleteLocation(id);
+  }
 }
