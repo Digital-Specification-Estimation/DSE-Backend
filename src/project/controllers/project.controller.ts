86d9c559ce824265d91bb099ada97c4842b088be
MUGISHA-Pascal <mugishapascal2008@gmail.com>
2025-03-17 14:23:46 +0200

    adding the functionalities to the project module

--- src/project/controllers/project.controller.ts
+++ src/project/controllers/project.controller.ts
@@ -6,10 +6,33 @@ import {
   Patch,
   Param,
   Delete,
+  Put,
 } from '@nestjs/common';
 import { ProjectService } from '../services/project.service';
+import { CreateProjectDto } from '../dto/create-project.dto';
+import { UpdateProjectDto } from '../dto/update-project.dto';
 
 @Controller('project')
 export class ProjectController {
   constructor(private readonly projectService: ProjectService) {}
+  @Get('projects')
+  async getProjects() {
+    return this.projectService.getProjects();
+  }
+  @Get(':id')
+  async getProjectsById(@Param('id') id: string) {
+    return this.projectService.getProjectById(id);
+  }
+  @Post('add')
+  async addProject(@Body() createProject: CreateProjectDto) {
+    return this.projectService.addProject(createProject);
+  }
+  @Put('edit')
+  async updateProject(@Body() updateProject: UpdateProjectDto) {
+    return this.projectService.editProject(updateProject);
+  }
+  @Delete('delete')
+  async deleteProject(@Param('id') id: string) {
+    return this.projectService.deleteProject(id);
+  }
 }
--- src/project/services/project.service.ts
+++ src/project/services/project.service.ts
@@ -1,4 +1,46 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateProjectDto } from '../dto/create-project.dto';
+import { UpdateProjectDto } from '../dto/update-project.dto';
 
 @Injectable()
-export class ProjectService {}
+export class ProjectService {
+  constructor(private prisma: PrismaService) {}
+  async addProject(createProject: CreateProjectDto) {
+    return this.prisma.project.create({ data: createProject });
+  }
+  async deleteProject(id: string) {
+    if (await this.projectExists(id)) {
+      return this.prisma.project.delete({ where: { id } });
+    } else {
+      throw new NotFoundException('the project arleady exists');
+    }
+  }
+  async projectExists(id: string): Promise<boolean> {
+    const user = this.prisma.project.findUnique({ where: { id } });
+    return !!user;
+  }
+  async editProject(updateProject: UpdateProjectDto) {
+    if (!updateProject.id) {
+      throw new NotFoundException('the project id not found');
+    }
+    if (await this.projectExists(updateProject.id)) {
+      return this.prisma.project.update({
+        where: { id: updateProject.id },
+        data: { ...updateProject },
+      });
+    } else {
+      throw new NotFoundException('the project arleady exists');
+    }
+  }
+  async getProjects() {
+    return this.prisma.project.findMany();
+  }
+  async getProjectById(id: string) {
+    if (await this.projectExists(id)) {
+      return this.prisma.project.findUnique({ where: { id } });
+    } else {
+      throw new NotFoundException('the project arleady exists');
+    }
+  }
+}
