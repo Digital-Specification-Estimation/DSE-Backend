be158a31729af2b4ef3fd34c5db8e3efb9c32214
MUGISHA-Pascal <mugishapascal2008@gmail.com>
2025-03-29 20:13:05 +0200

    setting up the notificationgateway

--- src/notifications/gateways/notifications.gateway.ts
+++ src/notifications/gateways/notifications.gateway.ts
@@ -2,32 +2,42 @@ import {
   WebSocketGateway,
   SubscribeMessage,
   MessageBody,
+  OnGatewayConnection,
+  OnGatewayInit,
+  OnGatewayDisconnect,
 } from '@nestjs/websockets';
 import { NotificationsService } from '../services/notifications.service';
 import { CreateNotificationDto } from '../dto/create-notification.dto';
 import { UpdateNotificationDto } from '../dto/update-notification.dto';
-
-@WebSocketGateway()
-export class NotificationsGateway {
-  constructor(private readonly notificationsService: NotificationsService) {}
-
-  @SubscribeMessage('createNotification')
-  create(@MessageBody() createNotificationDto: CreateNotificationDto) {
-    return this.notificationsService.create(createNotificationDto);
+import { PrismaService } from 'src/prisma/prisma.service';
+import { Server, Socket } from 'socket.io';
+@WebSocketGateway({ cors: '*' })
+export class NotificationsGateway
+  implements OnGatewayConnection, OnGatewayInit, OnGatewayDisconnect
+{
+  constructor(
+    private readonly notificationsService: NotificationsService,
+    private prisma: PrismaService,
+  ) {}
+  private clients = new Map<string, Socket>();
+  afterInit(server: Server) {
+    console.log('websocket server initialized');
   }
-
-  @SubscribeMessage('findAllNotifications')
-  findAll() {
-    return this.notificationsService.findAll();
+  handleConnection(client: Socket) {
+    console.log(`Client connected : ${client.id}`);
+    this.clients.set(client.id, client);
+    client.emit('connection_status', 'Connected to websocket server');
   }
-
-  @SubscribeMessage('findOneNotification')
-  findOne(@MessageBody() id: number) {
-    return this.notificationsService.findOne(id);
+  handleDisconnect(client: Socket) {
+    console.log(`Client disconnected: ${client.id}`);
+    this.clients.delete(client.id);
   }
-
-  @SubscribeMessage('removeNotification')
-  remove(@MessageBody() id: number) {
-    return this.notificationsService.remove(id);
+  async sendNotification(user_id: string, message: string) {
+    await this.prisma.notification.create({
+      data: { user_id, message, read: false },
+    });
+    for (const client of this.clients.values()) {
+      client.emit('notification', message);
+    }
   }
 }
--- src/notifications/notifications.module.ts
+++ src/notifications/notifications.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { NotificationsService } from './services/notifications.service';
 import { NotificationsGateway } from './gateways/notifications.gateway';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
-  providers: [NotificationsGateway, NotificationsService],
+  imports: [PrismaModule],
+  providers: [NotificationsGateway, NotificationsService, PrismaService],
 })
 export class NotificationsModule {}
