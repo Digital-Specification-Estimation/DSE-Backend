db4e5fc0fe612f9bb3ba0cf250d42048a55b550c
MUGISHA-Pascal <mugishapascal2008@gmail.com>
2025-04-02 22:36:53 +0200

    fix : deployment

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:3000', 'https://digitalestimation.vercel.app/'],
+    origin: ['http://localhost:3000', 'https://digitalestimation.vercel.app'],
     credentials: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type,Authorization',
