d32905b6ac0c9eaf92c789c8f413d6ef673a7785
MUGISHA-Pascal <mugishapascal2008@gmail.com>
2025-04-20 21:29:50 +0200

    fix : schema change

---
+++ prisma/migrations/20250420182947_adding_defaults_for_the_user_table/migration.sql
@@ -0,0 +1,4 @@
+-- AlterTable
+ALTER TABLE "User" ALTER COLUMN "currency" SET DEFAULT 'USD',
+ALTER COLUMN "payslip_format" SET DEFAULT 'PDF',
+ALTER COLUMN "salary_calculation" SET DEFAULT 'daily rate';
