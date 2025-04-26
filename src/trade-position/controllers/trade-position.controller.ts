879bab1fb82e1ca1c45bf736d3bd85c16ce3522e
MUGISHA-Pascal <mugishapascal2008@gmail.com>
2025-04-26 21:13:05 +0200

    fix : days calculation fix

--- src/trade-position/controllers/trade-position.controller.ts
+++ src/trade-position/controllers/trade-position.controller.ts
@@ -48,7 +48,9 @@ export class TradePositionController {
     );
 
     const timeDifference = startOfToday.getTime() - startOfPastDay.getTime();
-    return Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+    const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+
+    return days + 1; // Add 1 to include today
   }
 
   @Get('trades')
